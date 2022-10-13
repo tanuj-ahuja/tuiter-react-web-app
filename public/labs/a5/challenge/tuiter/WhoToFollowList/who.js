fce11295d14965de231cf3441238176295b5d1f5
Tanuj Ahuja <tanuj.ahuja@postman.com>
2022-10-12 20:52:28 -0400

    tweaks

--- public/labs/a5/challenge/tuiter/WhoToFollowList/who.js
+++ public/labs/a5/challenge/tuiter/WhoToFollowList/who.js
@@ -1,14 +1,14 @@
 export default [
     {   avatarIcon: '../../images/java.png',
-        userName: 'Java', handle: 'Java', },
+        userName: 'Java', handle: '@Java', },
     {   avatarIcon: '../../images/space.png',
         userName: 'Relativity Space',
-        handle: 'relativityspace', },
+        handle: '@relativityspace', },
     {   avatarIcon: '../../images/virgin-1.svg',
         userName: 'Virgin Galactic',
-        handle: 'virgingalactic', },
+        handle: '@virgingalactic', },
     {   avatarIcon: '../../images/nasa.png',
-        userName: 'NASA', handle: 'NASA', },
+        userName: 'NASA', handle: '@NASA', },
     {   avatarIcon: '../../images/tesla.png',
-        userName: 'Tesla', handle: 'Tesla', }, ];
+        userName: 'Tesla', handle: '@Tesla', }, ];
 
