c09ae41eb2eeb915260f45bf70cda0ac4f47c708
Tanuj Ahuja <tanuj.ahuja@postman.com>
2022-10-14 20:38:55 -0400

    a6 lab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-
-import './vendors/bootstrap/css/bootstrap.min.css';
-import './vendors/bootstrap/bootstrap.min.css';
-import './vendors/fontawesome/css/all.min.css';
+import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
-  return (
-      <div className="container">
-        <h1>Hello World!</h1>
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                <Route index element = {<Labs/>}/>
+                <Route path="/hello" element={<HelloWorld/>}/>
+                <Route path="/tuiter" element={<Tuiter/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
-
 export default App;
-
